b4b019dd0c649fe53ef6dfcd98d81028f2a91ac9
labexams <86876820+labexams@users.noreply.github.com>
2021-07-05 19:52:40 +0530

    Update JAVAGUI.java

--- JAVAGUI.java
+++ JAVAGUI.java
@@ -6,19 +6,20 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-
 public class JAVAGUI {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         ADDSUB a = new ADDSUB();
     }
 }
 
-class ADDSUB extends JFrame{
-    JTextField t1, t2;
-    JButton b1, b2;
+class ADDSUB extends JFrame implements ActionListener
+{
+    JTextField t1,t2;
+    JButton b1,b2;
     JLabel l;
-
-    public ADDSUB() {
+    public ADDSUB()
+    {
 
         t1 = new JTextField(10);
         t2 = new JTextField(10);
@@ -33,24 +34,25 @@ class ADDSUB extends JFrame{
         add(l);
 
 
-        b1.addActionListener(this);
-        b2.addActionListener(this);
+       b1.addActionListener(this);
+       b2.addActionListener(this);
         setLayout(new FlowLayout());
         setVisible(true);
-        setSize(400, 400);
+        setSize(400,400);
         setDefaultCloseOperation(3);
 
 
-    }
 
-    public void actionPerformed(ActionEvent ae) {
+    }
+    public void actionPerformed(ActionEvent ae)
+    {
         int num1 = Integer.parseInt(t1.getText());
         int num2 = Integer.parseInt(t2.getText());
         int value;
-        if (ae.getSource() == b1)
+        if(ae.getSource()==b1)
             value = num1 + num2;
         else
-            value = num1 - num2;
-        l.setText(value + "");
+            value = num1-num2;
+        l.setText(value+"");
     }
 }
