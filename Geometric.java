5aefb03a4413076f8e71368f3cfa0fc2c3533486
labexams <86876820+labexams@users.noreply.github.com>
2021-07-05 16:41:10 +0530

    Update Geometric.java

--- Geometric.java
+++ Geometric.java
@@ -1,68 +1,55 @@
 
 
-interface GeometricShape {
-    void area();
+package com.vishal;
 
+interface Geometricshape{
     void perimeter();
+    void area();
 }
-
-class Triangle implements GeometricShape {
-    double a = 1, b = 2, c = 1, h = 4;
-
+class Triangle implements Geometricshape {
+    double a = 1, b = 2, c = 1, h = 4,ar,per;
     public void area() {
-        System.out.println("");
-        double area1 = 0.5 * b * h;
-        System.out.println("Area of a Triangle: " + area1);
+        ar = 0.5*b*h;
+        System.out.println("Area of a Triangle:"+ar);
     }
-
     public void perimeter() {
-        double per = a + b + c;
-        System.out.println("Perimeter of a Triangle: " + per);
-        System.out.println("");
+        per = a+b+c;
+        System.out.println("Perimeter of Triangle:"+per);
     }
 }
-
-class Rectangle implements GeometricShape {
-    int length = 15, width = 10, per, area1;
-
+class rectangle implements Geometricshape {
+    int length = 15,width=10,per,ar;
     public void area() {
-        area1 = length * width;
-        System.out.println("Area of Rectangle: " + area1);
+        ar = length*width;
+        System.out.println("Area of rectangle:"+ar);
     }
-
     public void perimeter() {
-        per = 2 * length + 2 * width;
-        System.out.println("Perimeter of Rectangle: " + per);
-        System.out.println("");
+        per = (2*length) + (2 * width);
+        System.out.println("Perimeter of rectangle:"+per);
     }
 }
-
-class Circle implements GeometricShape {
+class circle implements Geometricshape {
     int r = 10;
-    double p = 3.141, area1 = 0.0, per = 0.0;
-
+    double p=3.14,ar=0.0,per=0.0;
     public void area() {
-        area1 = p * r * r;
-        System.out.println("Area of Circle: " + area1);
+        ar=p*r*r;
+        System.out.println("Area of rectangle:"+ar);
     }
-
     public void perimeter() {
-        per = 2 * p * r;
-        System.out.println("Perimeter of Circle: " + per);
-        System.out.println("");
+        per = 2*p*r;
+        System.out.println("Perimeter of rectangle:"+per);
     }
 }
-
-class Geometric  {
-    public static void main(String args[]) {
+public class Geometric {
+    public static void main(String[] args) {
         Triangle t = new Triangle();
+        rectangle r = new rectangle();
+        circle c = new circle();
         t.area();
         t.perimeter();
-        Circle c = new Circle();
-        c.area();
-        c.perimeter();
-        Rectangle r = new Rectangle();
         r.area();
         r.perimeter();
+        c.area();
+        c.perimeter();
     }
 }
